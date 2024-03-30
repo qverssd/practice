3cb0a322a7b7feb0e94a907b07326ca292d8ea46
qverssd <aleksandr082005@gmail.com>
2024-03-30 19:46:19 +0200

    script.js changed

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
-const numberOfFilms =  +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genre: [],
-  privat: false
-};
+// const numberOfFilms =  +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genre: [],
+//   privat: false
+// };
 
- for(let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '')
+//  for(let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', '')
 
       
 
-      if(a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done')
-      } else {
-        console.log('error')
-        i--
-      }
+//       if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//       } else {
+//         console.log('error')
+//         i--
+//       }
 
-      if(personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов")
-      } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель")
-      } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман")
-      } else { 
-        console.log("Произошла ошибка")
-      }
+//       if(personalMovieDB.count < 10) {
+//         console.log("Просмотрено довольно мало фильмов")
+//       } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log("Вы классический зритель")
+//       } else if (personalMovieDB.count >= 30) {
+//         console.log("Вы киноман")
+//       } else { 
+//         console.log("Произошла ошибка")
+//       }
       
- }
+//  }
 
- console.log(personalMovieDB);
+//  console.log(personalMovieDB);
+
+
+
+"use strict"
+
+// let num = 20;
+
+// function showFirstMessage(text){
+//   console.log(text);
+//   let num = 10;
+//   console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+// function calc(a, b){
+//   return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret(){
+//   let num = 50;
+//   return num;
+// } 
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function(){
+//   console.log('Hello World!');
+// };
+
+// logger();
+
+// const calc = (a, b) => {return a + b};
 
